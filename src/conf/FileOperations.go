a5e1253d6132ed469e5030764c377c5a6f0625f9
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-04-20 14:08:44 +0200

    checking pieces: refactoring + comments

--- src/conf/FileOperations.go
+++ src/conf/FileOperations.go
@@ -12,13 +12,18 @@ import (
 	"io/ioutil"
 	"strings"
 )
-var partSize int
-var partBuffer []byte
 var totalPartsNum uint64
 const fileChunk = 1*(1<<10) // 1 KB
 var mainFileHash string
 
+
+/*
+SplitFile opens the file we want to split, computes the original hash,
+and saves each chunk as a new file following the fashion "bigFile_0_"
+@param path to the file we want to split
+*/
 func SplitFile(filePath string){
+	// Open the file we want to split
 	file, err:=os.Open(filePath)
 	if err != nil {
 		fmt.Println(err)
@@ -26,6 +31,7 @@ func SplitFile(filePath string){
 	}
 	defer file.Close()
 
+	// Compute original hash
 	hash := md5.New()
 	_, err = io.Copy(hash,file)
 	if err != nil {
@@ -54,18 +60,15 @@ func SplitFile(filePath string){
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("______________________________________n bytes read: ",len(partBuffer)," out of: ",partSize)
-		fmt.Printf("first char is: %s\n", string(partBuffer[1]))
-		fmt.Println(string(partBuffer))
 
-		// write to disk
+		// create new file
 		newFileName= path.Base(filePath)+"_"+strconv.FormatUint(i,10)+"_"
 		_, err =  os.Create(newFileName)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		// write / save buffer to disk
+		// write / save buffer to file
 		ioutil.WriteFile(newFileName, partBuffer, os.ModeAppend)
 		fmt.Println("Split to: ",newFileName)
 
@@ -74,6 +77,11 @@ func SplitFile(filePath string){
 
 }
 
+/*
+md5sum opens the file we want to compute the hash and computes it
+@param path to the file we want to split
+returns the computed hash
+*/
 func md5sum(filePath string) string{
 	file, err:=os.Open(filePath)
 	if err != nil {
@@ -89,19 +97,26 @@ func md5sum(filePath string) string{
 		return ""
 	}
 	mainFileHash:=hex.EncodeToString(hash.Sum(nil))
-	//fmt.Println(mainFileHash)
 	return mainFileHash
 }
 
+/*
+CheckPieces walks through the subfiles directory, creates a new file to be filled out with the content of each subfile,
+and compares the new hash with the original one.
+@param path to the file we want to split
+Returns true if both hash are identic and false if not
+*/
 func CheckPieces(fileName string) bool{
 
-
+	// Subfiles directory
 	path:=os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/"
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		fmt.Println(err)
 		return false
 	}
+
+	// Create new file
 	_, err =  os.Create(os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/z"+fileName)
 	newFile, err:=os.OpenFile(os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/z"+fileName,os.O_APPEND|os.O_WRONLY,0666)
 	if err != nil {
@@ -109,12 +124,13 @@ func CheckPieces(fileName string) bool{
 		return false
 	}
 	defer newFile.Close()
+
+	// Trying to fill out the new file using subfiles (in order)
 	var inOrderCount uint64=0
 	for inOrderCount<totalPartsNum {
 		for _, file := range files {
 			if strings.Compare(file.Name(), fileName + "_" + strconv.FormatUint(inOrderCount,10) + "_") == 0 {
 				inOrderCount++
-				fmt.Println("-------------------------- ****file:= " + file.Name())
 
 				currentFile, err := os.Open(path + file.Name())
 				if err != nil {
@@ -122,20 +138,22 @@ func CheckPieces(fileName string) bool{
 					return false
 				}
 				defer currentFile.Close()
+
 				bytesCurrentFile, err := ioutil.ReadFile(path + file.Name())
-				fmt.Println("bytes current file: ", string(bytesCurrentFile))
+
 				_, err= newFile.WriteString(string(bytesCurrentFile))
 				if err != nil {
-					panic(err)
+					fmt.Println(err)
+					return false
 				}
-				//ioutil.WriteFile(path+"z"+fileName, bytesCurrentFile, os.ModeAppend)
-				//newFile.Write(bytesCurrentFile)
+
 			}
 		}
 	}
+
+	// Compute and compare new hash
 	newHash:=md5sum(path+"/z"+fileName)
-	fmt.Println(mainFileHash)
-	fmt.Println(newHash)
+
 	if strings.Compare(mainFileHash,newHash)!=0 {return false}
 	return true
 }
