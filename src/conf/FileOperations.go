5d3e039df9fb59082b006d14480e0bac17810241
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-04-20 13:09:46 +0200

    checking pieces: zbigFile fill out

--- src/conf/FileOperations.go
+++ src/conf/FileOperations.go
@@ -14,6 +14,7 @@ import (
 )
 var partSize int
 var partBuffer []byte
+var totalPartsNum uint64
 const fileChunk = 1*(1<<10) // 1 KB
 
 func SplitFile(filePath string){
@@ -36,19 +37,28 @@ func SplitFile(filePath string){
 	fileInfo, _ := file.Stat()
 	var fileSize int64 = fileInfo.Size()
 	// calculate total number of parts the file will be chunked into
-	totalPartsNum:= uint64(math.Ceil(float64(fileSize)/float64(fileChunk)))
+	totalPartsNum= uint64(math.Ceil(float64(fileSize)/float64(fileChunk)))
 	fmt.Printf("File size= %d  Bytes\n",fileSize)
 	fmt.Printf("Splitting to %d pieces \n", totalPartsNum)
 	var partSize int
 	var partBuffer []byte
 	var newFileName string
+	allBytes,_:=ioutil.ReadFile(filePath)
+	fmt.Println(len(allBytes))
 	for i := uint64(0); i<totalPartsNum; i++{
 		partSize=int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
 		partBuffer=make([]byte,partSize)
-		file.Read(partBuffer)
+		partBuffer=allBytes[:partSize]
+		allBytes=allBytes[partSize:]
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("______________________________________n bytes read: ",len(partBuffer)," out of: ",partSize)
+		fmt.Printf("first char is: %s\n", string(partBuffer[1]))
+		fmt.Println(string(partBuffer))
 
 		// write to disk
-		newFileName= path.Base(filePath)+"_"+strconv.FormatUint(i,10)
+		newFileName= path.Base(filePath)+"_"+strconv.FormatUint(i,10)+"_"
 		_, err =  os.Create(newFileName)
 		if err != nil {
 			fmt.Println(err)
@@ -91,31 +101,35 @@ func CheckPieces(fileName string) bool{
 		fmt.Println(err)
 		return false
 	}
-	_, err =  os.Create(os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/"+fileName)
-	newFile, err:=os.Open(os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/"+fileName)
+	_, err =  os.Create(os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/z"+fileName)
+	newFile, err:=os.OpenFile(os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/z"+fileName,os.O_APPEND|os.O_WRONLY,0666)
 	if err != nil {
 		fmt.Println(err)
 		return false
 	}
-	defer newFile.Close()
-	for _, file  := range files {
-		if strings.Contains(file.Name(), fileName){
-			fmt.Println("file:= "+file.Name())
+	//defer newFile.Close()
+	var inOrderCount uint64=0
+	for inOrderCount<totalPartsNum {
+		for _, file := range files {
+			if strings.Compare(file.Name(), fileName + "_" + strconv.FormatUint(inOrderCount,10) + "_") == 0 {
+				inOrderCount++
+				fmt.Println("-------------------------- ****file:= " + file.Name())
 
-			currentFile, err:=os.Open(path+file.Name())
-			if err != nil {
-				fmt.Println(err)
-				return false
+				currentFile, err := os.Open(path + file.Name())
+				if err != nil {
+					fmt.Println(err)
+					return false
+				}
+				defer currentFile.Close()
+				bytesCurrentFile, err := ioutil.ReadFile(path + file.Name())
+				fmt.Println("bytes current file: ", string(bytesCurrentFile))
+				_, err= newFile.WriteString(string(bytesCurrentFile))
+				if err != nil {
+					panic(err)
+				}
+				//ioutil.WriteFile(path+"z"+fileName, bytesCurrentFile, os.ModeAppend)
+				//newFile.Write(bytesCurrentFile)
 			}
-			defer newFile.Close()
-
-			partSize=int(fileChunk)
-			partBuffer=make([]byte,partSize)
-			currentFile.Read(partBuffer)
-
-			//contentBytes,_:=ioutil.ReadFile(path+file.Name())
-			newFile.Write(partBuffer)
-			fmt.Println(hex.EncodeToString(partBuffer))
 		}
 	}
 
