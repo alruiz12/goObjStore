182ed256caa133a2e66a4344f035ea3c48781c37
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-07-11 12:47:26 +0200

    testing checkpieces

--- src/httpGo/httpGo_test.go
+++ src/httpGo/httpGo_test.go
@@ -9,84 +9,132 @@ import (
 )
 func TestDirecories(t *testing.T){
 	router :=MyNewRouter()
-
-
-
+	const IP = "127.0.0.1"
 
 
 	var filePath = os.Getenv("GOPATH")+"/src/github.com/alruiz12/simpleBT/dataset.xml"
 
+	// TRACKER
+	var trackerAddr = IP+":8000"
 
-	var proxyAddr = "127.0.0.1:8080"
-	var trackerAddr = "127.0.0.1:8070"
+	// PEERS
+	// IMPORTANT NOTE: last character of port is the peer's internal identifier
+	var Peer1a = IP+":8011"
+	var Peer1b = IP+":8021"
+	var Peer1c = IP+":8031"
+	var peer1List = []string{Peer1a, Peer1b, Peer1c}
 
-	// tracker sends to:
-	var Peer1 = "127.0.0.1:8081"
-	var Peer2 = "127.0.0.1:8082"
-	var Peer3 = "127.0.0.1:8083"
-	var Peer4 = "127.0.0.1:8084"
-	var Peer5 = "127.0.0.1:8085"
-	var Peers =[]string{Peer1, Peer2, Peer3, Peer4, Peer5}
-	// last character of port is the peer's internal identifier
+	var Peer2a = IP+":8012"
+	var Peer2b = IP+":8022"
+	var Peer2c = IP+":8032"
+	var peer2List = []string{Peer2a, Peer2b, Peer2c}
 
-	//var proxy1Addr = "127.0.0.1:8070"
-	var proxy2Addr = "127.0.0.1:8071"
-	var proxy3Addr = "127.0.0.1:8072"
-	var proxy4Addr = "127.0.0.1:8073"
-	var proxy5Addr = "127.0.0.1:8074"
+	var Peer3a = IP+":8013"
+	var Peer3b = IP+":8023"
+	var Peer3c = IP+":8033"
+	var peer3List = []string{Peer3a, Peer3b, Peer3c}
 
-	var ProxyAddr=[]string{/*proxy1Addr,*/proxy2Addr,proxy3Addr,proxy4Addr,proxy5Addr}
+	var Peers =[][]string{peer1List, peer2List, peer3List/*, peer4List, peer5List*/}
+
+	// PROXY
+	var proxy1 = IP+":8070"
+	var proxy2 = IP+":8071"
+	var proxy3 = IP+":8072"
+
+	var ProxyAddr=[]string{proxy1,proxy2,proxy3/*,proxy4,proxy5*/}
 
 
-	peer1:=&http.Server{Addr:":8081", Handler:router}
-	peer2:=&http.Server{Addr:":8082", Handler:router}
-	peer3:=&http.Server{Addr:":8083", Handler:router}
+
+
+
+
+	peer1arun:=&http.Server{Addr:Peer1a, Handler:router}
+	peer1brun:=&http.Server{Addr:Peer1b, Handler:router}
+	peer1crun:=&http.Server{Addr:Peer1c, Handler:router}
+	peer2arun:=&http.Server{Addr:Peer2a, Handler:router}
+	peer2brun:=&http.Server{Addr:Peer2b, Handler:router}
+	peer2crun:=&http.Server{Addr:Peer2c, Handler:router}
+	peer3arun:=&http.Server{Addr:Peer3a, Handler:router}
+	peer3brun:=&http.Server{Addr:Peer3b, Handler:router}
+	peer3crun:=&http.Server{Addr:Peer3c, Handler:router}
 	/*peer4:=&http.Server{Addr:"8084", Handler:router}
 	peer5:=&http.Server{Addr:"8085", Handler:router}
 	*/
 
-	proxy1:=&http.Server{Addr:":8071", Handler:router}
-	proxy2:=&http.Server{Addr:":8072", Handler:router}
-	proxy3:=&http.Server{Addr:":8073", Handler:router}
+	proxy1run:=&http.Server{Addr:proxy1, Handler:router}
+	proxy2run:=&http.Server{Addr:proxy2, Handler:router}
+	proxy3run:=&http.Server{Addr:proxy3, Handler:router}
 	/*proxy4:=&http.Server{Addr:"8074", Handler:router}
 	proxy5:=&http.Server{Addr:"8075", Handler:router}
 	*/
 
 
-	proxy:=&http.Server{Addr:":8080", Handler:router}
-	tracker:=&http.Server{Addr:":8070", Handler:router}
+
+	tracker:=&http.Server{Addr:trackerAddr, Handler:router}
 
 	go func(){
-		if err := peer1.ListenAndServe(); err!=nil{
+		if err := peer1arun.ListenAndServe(); err!=nil{
 			fmt.Println("ListenAndServe error", err.Error())
 		}
 	}()
 	go func(){
-		if err := peer2.ListenAndServe(); err!=nil{
+		if err := peer1brun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+	go func(){
+		if err := peer1crun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+
+	go func(){
+		if err := peer2arun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+	go func(){
+		if err := peer2brun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+	go func(){
+		if err := peer2crun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+
+	go func(){
+		if err := peer3arun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+	go func(){
+		if err := peer3brun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+	go func(){
+		if err := peer3crun.ListenAndServe(); err!=nil{
+			fmt.Println("ListenAndServe error", err.Error())
+		}
+	}()
+
+	go func(){
+		fmt.Println("go func")
+		if err := proxy1run.ListenAndServe(); err!=nil{
 			fmt.Println("ListenAndServe error", err.Error())
 		}
 	}()
 	go func(){
 		fmt.Println("go func")
-		if err := peer3.ListenAndServe(); err!=nil{
+		if err := proxy2run.ListenAndServe(); err!=nil{
 			fmt.Println("ListenAndServe error", err.Error())
 		}
 	}()
 	go func(){
 		fmt.Println("go func")
-		if err := proxy1.ListenAndServe(); err!=nil{
-			fmt.Println("ListenAndServe error", err.Error())
-		}
-	}()
-	go func(){
-		fmt.Println("go func")
-		if err := proxy2.ListenAndServe(); err!=nil{
-			fmt.Println("ListenAndServe error", err.Error())
-		}
-	}()
-	go func(){
-		fmt.Println("go func")
-		if err := proxy3.ListenAndServe(); err!=nil{
+		if err := proxy3run.ListenAndServe(); err!=nil{
 			fmt.Println("ListenAndServe error", err.Error())
 		}
 	}()
@@ -103,37 +151,64 @@ func TestDirecories(t *testing.T){
 		}
 	}()
 
+	StartTracker(Peers)
 
-	StartTracker(Peers, ProxyAddr)
 	go func(){
+		Put(filePath, trackerAddr, 3)
+
 		time.Sleep(5*time.Second)
-		Put(filePath, proxyAddr, trackerAddr, 3)
-		time.Sleep(5*time.Second)
+
 		Get("0527cbea2805d89c6d5d6457b7f9f77c",ProxyAddr, trackerAddr)
+
+		time.Sleep(45*time.Second)
+
+		if (CheckPieces("0527cbea2805d89c6d5d6457b7f9f77c","NEW.xml",filePath, 3)) == false {
+			t.Error("True expected")
+		}
 	}()
 
-	time.AfterFunc(150 * time.Second, func(){
-		if err:= peer1.Shutdown(nil); err!=nil{
+	time.AfterFunc(600 * time.Second, func(){
+		if err:= peer1arun.Shutdown(nil); err!=nil{
 			panic(err)
 		}
-		if err:= peer2.Shutdown(nil); err!=nil{
+		if err:= peer2arun.Shutdown(nil); err!=nil{
 			panic(err)
 		}
-		if err:= peer3.Shutdown(nil); err!=nil{
+		if err:= peer3arun.Shutdown(nil); err!=nil{
 			panic(err)
 		}
-		if err:= proxy1.Shutdown(nil); err!=nil{
+
+		if err:= peer1brun.Shutdown(nil); err!=nil{
 			panic(err)
 		}
-		if err:= proxy2.Shutdown(nil); err!=nil{
+		if err:= peer2brun.Shutdown(nil); err!=nil{
 			panic(err)
 		}
-		if err:= proxy3.Shutdown(nil); err!=nil{
+		if err:= peer3brun.Shutdown(nil); err!=nil{
 			panic(err)
 		}
-		if err:= proxy.Shutdown(nil); err!=nil{
+
+		if err:= peer1crun.Shutdown(nil); err!=nil{
 			panic(err)
 		}
+		if err:= peer2brun.Shutdown(nil); err!=nil{
+			panic(err)
+		}
+		if err:= peer3crun.Shutdown(nil); err!=nil{
+			panic(err)
+		}
+
+
+		if err:= proxy1run.Shutdown(nil); err!=nil{
+			panic(err)
+		}
+		if err:= proxy2run.Shutdown(nil); err!=nil{
+			panic(err)
+		}
+		if err:= proxy3run.Shutdown(nil); err!=nil{
+			panic(err)
+		}
+
 		if err:= tracker.Shutdown(nil); err!=nil{
 			panic(err)
 		}
