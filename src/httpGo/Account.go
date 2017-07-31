d921bec60fcc90d0bcda5bbbe21b3bf0af96a625
alruiz12 <alvaro-ruiz-95@hotmail.com>
2017-08-01 00:45:23 +0200

    renaming functions

--- src/httpGo/Account.go
+++ src/httpGo/Account.go
@@ -1,6 +1,5 @@
 package httpGo
 import (
-	"fmt"
 	"sync"
 )
 //var Accounts = make(map[string]Account)
@@ -8,17 +7,6 @@ var Accounts = struct{
 	m map[string]Account
 	sync.Mutex
 }{m: make(map[string]Account) }
-func CreateAccount2 (name string){
-	Accounts.Lock()
-	_, exists := Accounts.m[name]
-	if !exists {
-		m := make(map[string]Container)
-		Accounts.m[name]=Account{Containers:m, Name:name}
-		fmt.Println("Account ", name, " created")
-	}
-	Accounts.Unlock()
-
-}
 
 type Account struct {
 	Name string
